9039f73290a54ac252cdb8b289dd33e1e0ef630d
yiwent <541051236@qq.com>
2017-03-22 00:33:40 +0800

    UIWidgeTest

---
+++ chapter3/UIWidgeTest/app/src/main/java/com/yiwen/uiwidgetest/PercentActivity.java
@@ -0,0 +1,13 @@
+package com.yiwen.uiwidgetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PercentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_percent);
+    }
+}
