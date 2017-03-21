e99946941751eeab557b8a63ddb11b5cb61f3d9a
yiwent <541051236@qq.com>
2017-03-21 22:21:32 +0800

    activitylifecycletest

---
+++ chapter2/Activitylifecycletest/app/src/main/java/com/yiwen/activitylifecycletest/MainActivity.java
@@ -0,0 +1,82 @@
+package com.yiwen.activitylifecycletest;
+
+import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+public static final String TAG="MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState!=null){
+            String tempData=savedInstanceState.getString("DataKey");
+            Log.d(TAG, "onCreate: "+tempData);
+        }
+        Button Normal_bt= (Button) findViewById(R.id.button);
+        Button Dialog_bt= (Button) findViewById(R.id.button2);
+        Normal_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Nomal_Activity.class);
+                startActivity(intent);
+            }
+        });
+        Dialog_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,DialogActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        String tempData="something need to save";
+        outState.putString("DataKey",tempData);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+}
---
+++ chapter2/Activitylifecycletest/app/src/main/java/com/yiwen/activitylifecycletest/Nomal_Activity.java
@@ -0,0 +1,13 @@
+package com.yiwen.activitylifecycletest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nomal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nomal);
+    }
+}
