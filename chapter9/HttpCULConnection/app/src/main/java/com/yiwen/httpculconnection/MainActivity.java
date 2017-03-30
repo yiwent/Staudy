c06dc8d0c50d12f33e24564636433b820695a4d9
yiwent <541051236@qq.com>
2017-03-30 11:01:56 +0800

    HttpCULConnection

---
+++ chapter9/HttpCULConnection/app/src/main/java/com/yiwen/httpculconnection/MainActivity.java
@@ -0,0 +1,82 @@
+package com.yiwen.httpculconnection;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView responseText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button send = (Button) findViewById(R.id.bt_send);
+        responseText = (TextView) findViewById(R.id.respone_text);
+        send.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.bt_send) {
+            sendRequestwithHttpURLConnection();
+        }
+    }
+
+    private void sendRequestwithHttpURLConnection() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                try {
+                    URL url = new URL("https://www.baidu.com");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    //读取
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line=reader.readLine())!=null) {
+                        response.append(line);
+                    }
+                    showResponse(response.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (reader!=null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (connection!=null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void showResponse(final String response) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                responseText.setText(response);
+            }
+        });
+    }
+}
